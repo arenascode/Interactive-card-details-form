29a58929f8eacce742e5f965ef7cc4fd32d8e94c
Miguel Arenas <migue.arenas91@gmail.com>
2022-11-18 11:26:51 -0300

    maniputaling DOM in process

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 // Variables 
 
 const cvcCard = document.getElementById('cvcContent')
+const cardFrontNumber = document.getElementById('frontCardNumber')
+const frontCardName = document.getElementById('frontCardName')
+const expiredDateCard = document.getElementById('expireDate')
+// Form Inputs
+const cardHolderNameInput = document.getElementById('buyerName');
+const cardNumberInput = document.getElementById('cardNumber')
+const expireMonth = document.getElementById("expireMonth")
+const expireYear = document.getElementById("expireYear")
+const cvcInput = document.getElementById('cvcInput')
 
-cvcCard.innerText = '000'
+cardHolderNameInput.oninput = function (e) {
+  frontCardName.innerText = e.target.value
+}
+cardNumberInput.oninput = function (e) {
+  cardFrontNumber.innerText = e.target.value
+}
+cvcInput.oninput = function (e) {
+  console.log(e.target.value);
+  cvcCard.innerText = e.target.value;
+}
+expireMonth.oninput = function (e) {
+  expiredDateCard.innerText = e.target.value;
+}
+// separate the month of the year in front card
