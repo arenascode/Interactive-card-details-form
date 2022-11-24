66d060133ca669822c7e28f16cd9781e69ff1047
Miguel Arenas <migue.arenas91@gmail.com>
2022-11-24 15:34:44 -0300

    logic and responsive for mobile almost done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const expireMonthCard = document.getElementById('expireMonth')
 const expireYearCard = document.getElementById("expireYear");
 // Form Inputs
 const inputs = document.querySelectorAll('input')
+const errorInputs = document.querySelectorAll(".invalid");
 
 
 // write in Credit card the values of inputs
@@ -14,13 +15,17 @@ inputs.forEach(input => {
   input.addEventListener('keyup', () => {
     //Name Input
     if (inputs[0].value !== '') {
-      frontCardName.innerText = inputs[0].value.toUpperCase()
+      frontCardName.innerText = inputs[0].value.toUpperCase();
+      errorInputs[0].innerText = "";
+      inputs[0].style.borderColor = "grey";
     } if (inputs[0].value.length == 0) {
       frontCardName.innerText = 'xxxx xxxxx '
     }
     // cardNumber Input
     if (inputs[1].value !== "") {
       frontCardNumber.innerText = inputs[1].value;
+      errorInputs[1].innerText = "";
+      inputs[1].style.borderColor = "grey";
     }
     if (inputs[1].value.length == 0) {
       frontCardNumber.innerText = "0000 0000 0000 0000 ";
@@ -28,6 +33,8 @@ inputs.forEach(input => {
     //EXP. Month Input
     if (inputs[2].value !== "") {
       expireMonthCard.innerText = inputs[2].value;
+      errorInputs[2].innerText = "";
+      inputs[2].style.borderColor = "grey";
     }
     if (inputs[2].value.length == 0) {
       expireMonthCard.innerText = "00";
@@ -35,6 +42,8 @@ inputs.forEach(input => {
    // EXP. Year Input
     if (inputs[3].value !== "") {
       expireYearCard.innerText = inputs[3].value;
+      errorInputs[2].innerText = "";
+      inputs[3].style.borderColor = "grey";
     }
     if (inputs[3].value.length == 0) {
       expireYearCard.innerText = "00";
@@ -42,12 +51,22 @@ inputs.forEach(input => {
   // CVC Input 
     if (inputs[4].value !== "") {
       cvcCard.innerText = inputs[4].value;
+      errorInputs[3].innerText = "";
+      inputs[4].style.borderColor = "grey";
     }
     if (inputs[4].value.length == 0) {
       cvcCard.innerText = "000";
     }
   })
 })
+
+// Formatting the card number input
+inputs[1].addEventListener("input", function (e) {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
 
 
 // cardHolderNameInput.oninput = function (e) {
--- js/test.js
+++ js/test.js
@@ -2,10 +2,10 @@
 const inputs = document.querySelectorAll("input");
 const errorInputs = document.querySelectorAll(".invalid");
 console.log(errorInputs);
-let letters = /^[a-zA-Z]/g;
-
+let letters = /^[A-Za-z]+$/;
+console.log(inputs[1].value.match(letters));
 const form = document.getElementById("formCard");
-console.log(form);
+
 
 function validateForm() {
   form.addEventListener("submit", (e) => {
@@ -23,12 +23,40 @@ function validateForm() {
     } if (inputs[1].value.match(letters)) {
       errorInputs[1].innerText = 'Wrong format, numbers only';
        inputs[1].style.borderColor = "#ff5252";
-    } 
+    } if (inputs[1].value !== "" && inputs[1].value.length < 19) {
+      errorInputs[1].innerText = "Wrong format, must be 16 numbers";
+      inputs[1].style.borderColor = "#ff5252";
+    }
     // Validating month
     if (inputs[2].value.length > 2 || inputs[2].value > 12) {
-      errorInputs[2].innerText = "ingresa un formato valido";
+      errorInputs[2].innerText = "Enter a correct date";
+      inputs[2].style.borderColor = "#ff5252";
+    } if (inputs[2].value == "") {
+      errorInputs[2].innerText = `Can't be blank`;
+      inputs[2].style.borderColor = "#ff5252";
     }
-    console.log('The form was submited');
+    // Validating Year 
+    if (inputs[3].value.length > 2 || inputs[3].value < 0) {
+      errorInputs[2].innerText = "fill correct date";
+      inputs[3].style.borderColor = "#ff5252";
+    }
+    if (inputs[3].value == "") {
+      errorInputs[2].innerText = `Can't be blank`;
+      inputs[3].style.borderColor = "#ff5252";
+    }
+    // Validating cvc 
+    if (inputs[4].value == "") {
+      errorInputs[3].innerText = `Can't be blank`
+      inputs[4].style.borderColor = "#ff5252";
+    }
+    if (inputs[4].value.match(letters)) {
+      errorInputs[3].innerText = `Wrong Format, numbers only`;
+      inputs[4].style.borderColor = "#ff5252";
+    }
+    if (inputs[4].value !== "" && inputs[4].value.length < 3) {
+      errorInputs[3].innerText = `Wrong Format, must be 3 numbers`;
+    }
+      console.log("The form was submited");
   });
 }
 
